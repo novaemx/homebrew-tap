80ff47a9a624330a1d012f479f3dbaf5d312d931
Luis R. Lozano <llozanos81@users.noreply.github.com>
2026-05-04 12:40:37 -0600

    chore: checkpoint pending formula updates

--- Formula/gitflow.rb
+++ Formula/gitflow.rb
@@ -1,9 +1,9 @@
 class Gitflow < Formula
   desc "Git Flow workflow helper with interactive TUI and CLI"
   homepage "https://github.com/novaemx/gitflow-helper"
-  url "https://github.com/novaemx/gitflow-helper/releases/download/v0.6.1/gitflow-0.6.1-darwin-universal.tar.gz"
-  version "0.6.1"
-  sha256 "01997bd735be49a75333836c86a230a7381a59455059bb529aa21a8682a4cd3f"
+  url "https://github.com/novaemx/gitflow-helper/releases/download/v0.6.2/gitflow-0.6.2-darwin-universal.tar.gz"
+  version "0.6.2"
+  sha256 "adce8790cde0f069017b82302e093b8b40eabb41e38eb958ffe5da8017914c67"
   license "MIT"
 
   def install
